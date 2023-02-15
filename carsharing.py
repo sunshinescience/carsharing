4e55192394079998df52ea88a398ce78d329b08b
sunshinescience <sunshine.abbott@gmail.com>
2023-02-15 06:13:18 +0000

    added delete and put

--- carsharing.py
+++ carsharing.py
@@ -31,16 +31,42 @@ def car_by_id(id: int) -> dict:
     else:
         raise HTTPException(status_code=404, detail=f'No car with id {id}.')
 
-# @app.post("/api/cars/", response_model=CarOutput)
-# def add_car(car: CarInput) -> CarOutput:
-#     new_car = CarOutput(size=car.size, doors=car.doors,
-#                         fuel=car.fuel, transmission=car.transmission,
-#                         id=len(db)+1)
-#     db.append(new_car)
-#     save_db(db)
-#     return new_car
+# response_model (below) is an instruction to FastAPI about how our method should operate, so it should be a decorator argument instead of a function argument below
+@app.post("/api/cars/", response_model=CarOutput)
+def add_car(car: CarInput) -> CarOutput:
+    new_car = CarOutput(size=car.size, doors=car.doors,
+                        fuel=car.fuel, transmission=car.transmission,
+                        id=len(db)+1)
+    db.append(new_car)
+    save_db(db)
+    return new_car
 
 # Should the above car: have a type hint, it has CarInput, but is that wrong?
+
+# status_code=204 (below) is an instruction to FastAPI about how our method should operate, so it should be a decorator argument instead of a function argument below
+@app.delete("/api/cars/{id}", status_code=204)
+def remove_car(id: int) -> None:
+    matches = [car for car in db if car.id == id]
+    if matches:
+        car = matches[0]
+        db.remove(car)
+        save_db(db)
+    else:
+        raise HTTPException(status_code=404, detail=f'No car with id={id}.')
+
+@app.put("/api/cars/{id}", response_model=CarOutput)
+def change_car(id: int, new_data: CarInput) -> CarOutput: # The CarInput object here is a Pydantic object and that means it will be taken from the request body, like with a POST, it will contain the properties for the car
+    matches = [car for car in db if car.id == id]
+    if matches:
+        car = matches[0]
+        car.fuel = new_data.fuel
+        car.transmission = new_data.transmission
+        car.size = new_data.size
+        car.doors = new_data.doors
+        save_db(db)
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f'No car with id={id}.')
 
 if __name__ == "__main__":
     uvicorn.run("carsharing:app", reload=True)
--- schemas.py
+++ schemas.py
@@ -1,5 +1,4 @@
 import json
-from typing import List
 
 from pydantic import BaseModel
 from typing import Optional
@@ -12,20 +11,44 @@ from typing import Optional
 # Note that you can add default values so that these aren't necessary to include, for e.g., fuel and transmission
 # Note that Pydantic BaseModel expects a bunch of keyword arguments (e.g., size="s", doors="3", id="6")
 
+class TripInput(BaseModel):
+    start: int
+    end: int
+    description: str
+    
+
+class TripOutput(TripInput):
+    id: int
+
+
 class CarInput(BaseModel):
     size: str
     fuel: Optional[str] = "electric"
     doors: int
     transmission: Optional[str] = "auto"
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "size": "m",
+                "doors": 5,
+                "transmission": "manual",
+                "fuel": "hybrid"
+            }
+        }
+
 class CarOutput(CarInput):
     id: int
+    trips: list[TripOutput] = []
+
 
 def load_db() -> list[CarOutput]:
     """Load a list of Car objects from a JSON file"""
     with open("cars.json") as f:
         return [CarOutput.parse_obj(obj) for obj in json.load(f)]
 
+
 def save_db(cars: list[CarOutput]):
     with open("cars.json", 'w') as f:
         json.dump([car.dict() for car in cars], f, indent=4)
+
